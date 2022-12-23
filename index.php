4a9c276cd3c271d6cfb86e788236abb6857c4d4f
amirakhaileena <amira.khaileena@gmail.com>
2022-12-23 15:08:32 +0700

    menambakan paragraph

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <body>
   <h1>HELLO WORLD</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quisquam corporis labore ipsa. Temporibus quia a sit ipsum nulla necessitatibus harum blanditiis assumenda accusantium. Aliquid dolor ratione perspiciatis magni blanditiis?</p>
 </body>
 
 </html>
